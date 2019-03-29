ed5dfa95488782fa165b1de98fd10a1afc16d9ec
Bryan Lynch <bml312@nyu.edu>
2019-03-29 17:10:42 -0400

    initial frame

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    var canvas = document.getElementById('game-canvas');
+    canvas.height = 780;
+    canvas.width = 1400;
+    var ctx1 = canvas.getContext('2d');
+});
+
